47a3d7fee0ff29d2a361ac1ed07144c6ba1b9e17
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2025-08-05 21:59:21 +0530

    for loop, range

--- _07_for_loop/main.go
+++ _07_for_loop/main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i + 1)
+	// }
+
+	//Infinite Loop
+	// counter := 0
+	// for {
+	// 	fmt.Println("Infinite Loop")
+	// 	counter++
+
+	// 	if counter == 5 {
+	// 		break
+	// 	}
+	// }
+
+	// range
+	// numbers := []int{9, 8, 7, 6, 5}
+
+	// for index, value := range numbers {
+	// 	fmt.Println("Index :", index, "Value :", value)
+	// }
+
+	name := "Sumsum Gogoi"
+	for index, char := range name {
+		fmt.Printf("Index : %d, Char : %c \n", index, char)
+	}
 }
