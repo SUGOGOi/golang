284f5c5c2eea280ad561b082a6455cabccb41dad
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2025-08-29 09:30:33 +0530

    wait group

--- _23_goroutines/main.go
+++ _23_goroutines/main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
+	// "time"
 )
 
-// func task(id int) {
-// 	fmt.Println("Task :", id)
-// }
+func task(id int, w *sync.WaitGroup) {
+	defer w.Done()
+	fmt.Println("Task :", id)
+}
 
 func main() {
 
-	for i := 0; i <= 10; i++ {
-		// go task(i)
+	var wg sync.WaitGroup
 
-		go func(i int) {
-			fmt.Println(i)
-		}(i)
+	for i := 0; i <= 10; i++ {
+		wg.Add(1)
+		go task(i, &wg)
+
+		// go func(i int) {
+		// 	fmt.Println(i)
+		// }(i)
 	}
 
-	time.Sleep(time.Second * 2)
+	// time.Sleep(time.Second * 2)
+
+	wg.Wait()
+
 }
